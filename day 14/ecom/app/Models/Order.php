9cfcf4963696f329535dc78565cdaff0a9ba1a30
Jeselle Bacosmo <jbacosmo@gmail.com>
2021-09-09 09:59:53 -0700

    added model fillables

--- day 14/ecom/app/Models/Order.php
+++ day 14/ecom/app/Models/Order.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $table = 'orders';
+    protected $fillable = [
+        'user_id',
+        'reference_no',
+        'order_items',
+        'sub_total',
+        'delivery_fee',
+        'total',
+        'full_address',
+        'payment_id',
+        'payment_response',
+        'payment_status',
+        'status'
+    ]
 }
