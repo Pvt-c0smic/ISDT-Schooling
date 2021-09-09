0c1324d4292fd27511e0a0a4393680b9cc38f13a
Jeselle Bacosmo <jbacosmo@gmail.com>
2021-09-09 10:28:24 -0700

    added views

--- day 14/ecom/routes/admin/products.php
+++ day 14/ecom/routes/admin/products.php
@@ -1,1 +1,9 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\ProductsController;
+
+Route::get('/products', [
+    ProductsController::class,
+    'index'
+])->name('admin.products');
