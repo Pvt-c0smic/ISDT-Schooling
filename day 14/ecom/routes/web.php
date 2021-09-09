2549a81764d067c09a3a51def43bc6abf2add11e
Jeselle Bacosmo <jbacosmo@gmail.com>
2021-09-09 10:11:49 -0700

    added routing

--- day 14/ecom/routes/web.php
+++ day 14/ecom/routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::group(['prefix' => 'admin'], function() {
+    foreach(glob(__DIR__ ."/admin/*.php") as $filename) {
+        include $filename;
+    }
+})
+
+foreach (glob(__DIR__ . "/site/*.php") as $filename) {
+include $filename;
+}
